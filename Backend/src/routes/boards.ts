707d3b106b378fbbbeff841f841d96eb0fa4411e
kalhankrishna <kkgogoi28@gmail.com>
2026-01-15 20:45:04 +0530

    FB-020: Boards CRUD endpoints

--- Backend/src/routes/boards.ts
+++ Backend/src/routes/boards.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
-const Prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 // GET /api/boards/:id
 router.get("/:id", async(req, res)=>{
     try {
         const {id} = req.params;
     
-        const board = await Prisma.board.findUnique({
+        const board = await prisma.board.findUnique({
             where : {id},
             include: {
                 columns: {
@@ -32,6 +32,68 @@ router.get("/:id", async(req, res)=>{
     catch(err){
         console.error("Error fetching board: ", err);
         return res.status(500).json({error: "Internal server error"});
+    }
+});
+
+// POST /api/boards
+router.post("/", async(req, res)=>{
+    try{
+        const {name} = req.body;
+
+        if(!name){
+            return res.status(400).json({error: "Missing required field: name"});
+        }
+        const board = await prisma.board.create({
+            data: {name}
+        });
+        res.status(201).json(board);
+    }
+    catch(error){
+        console.error('Error creating board:', error);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+//PATCH /api/boards/:id
+router.patch("/:id", async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const {name} = req.body;
+        if(!name){
+            return res.status(400).json({error: "Missing required field: name"});
+        }
+        const updatedBoard = await prisma.board.update({
+            where: { id },
+            data: { name },
+        });
+        res.json(updatedBoard);
+    }
+    catch(error: any){
+        console.error('Error updating board:', error);
+
+        if (error.code === 'P2025') {
+            return res.status(404).json({ error: 'Board not found' });
+        }
+
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+// DELETE /api/boards/:id
+router.delete("/:id", async(req, res)=>{
+    try{
+        const {id} = req.params;
+        await prisma.board.delete({
+            where: {id}
+        });
+        res.status(204).send();
+    }
+    catch(error: any){
+        console.error('Error deleting board:', error);
+        if (error.code === 'P2025') {
+            return res.status(404).json({ error: 'Board not found' });
+        }
+        res.status(500).json({error: "Internal server error"});
     }
 });
 
